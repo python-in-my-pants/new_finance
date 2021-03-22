d78fdc522a83211f519a8ff7f8ea328fd55092ff
daniel_laptop <sora-no-isan@gmx.de>
2021-03-22 13:16:12 +0100

    first laptop push

--- Option strategy selector.py
+++ Option strategy selector.py
@@ -14,7 +14,6 @@ ACCOUNT_BALANCE = 2000
 
 
 def get_options_data(symbol):
-
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
     req = urllib.request.Request('https://www.barchart.com/stocks/quotes/' + symbol, headers=headers)
@@ -27,15 +26,15 @@ def get_options_data(symbol):
         next_earnings = ""
 
         for i, t in enumerate(text):
-            if "IV Rank" in t and "%" in text[i+2]:
-                ivr = float(text[i+2].replace("%",""))
+            if "IV Rank" in t and "%" in text[i + 2]:
+                ivr = float(text[i + 2].replace("%", ""))
             if "Volume Avg (30-Day)" in t and vol30 == 0:  # option volume
-                vol30 = int(text[i+2].replace(",", ""))
-            if "Open Int (30-Day)"in t and oi30 == 0:
-                oi30 = int(text[i+2].replace(",", ""))
+                vol30 = int(text[i + 2].replace(",", ""))
+            if "Open Int (30-Day)" in t and oi30 == 0:
+                oi30 = int(text[i + 2].replace(",", ""))
             if "Next Earnings Date" in t and not next_earnings:
-                #print(text[i+1], text[i+2], text[i+3])
-                next_earnings = text[i+2].replace(" ", "")
+                # print(text[i+1], text[i+2], text[i+3])
+                next_earnings = text[i + 2].replace(" ", "")
 
         return ivr, vol30, oi30, next_earnings
 
@@ -48,13 +47,15 @@ def get_underlying_outlook(symbol):
     ...
     # barchart.com ? (seems shitty)
     # RSI, VWAP, Bollinger
+    # use option chain data to get prob distribution of future price
+    #   https://financial-hacker.com/the-mechanical-turk/#more-2974
     return 1, 1, 1
+
 
 # type, positions, credit/debit, max risk, break even on exp, max prof, bpr, return on margin with 50% tp, close by date
 # use monto carlo sim for P50
 def propose_strategies(yf_obj, defined_risk_only=True, avoid_events=True,
                        min_vol30=25000, min_oi30=5000, strict_mode=False):
-
     # todo get short term price outlook (technical analysis, resistances from barchart.com etc)
 
     # <editor-fold desc="Get info">
@@ -83,14 +84,15 @@ def propose_strategies(yf_obj, defined_risk_only=True, avoid_events=True,
 
     # 2 # Price
 
-    if u_price*100 > ACCOUNT_BALANCE / 2:
-        print(f'Warning! Underlying asset price exceeds account risk tolerance! {u_price*100} > {ACCOUNT_BALANCE/2}')
+    if u_price * 100 > ACCOUNT_BALANCE / 2:
+        print(
+            f'Warning! Underlying asset price exceeds account risk tolerance! {u_price * 100} > {ACCOUNT_BALANCE / 2}')
         if strict_mode:
             return
 
     # 3 # upcoming events (dividends, earnings, splits) -> todo need to check for news still
 
-    if ...: # upcoming event
+    if ...:  # upcoming event
         if avoid_events:
             # restrict expirations to those before earnings and resume
             ...
@@ -129,9 +131,58 @@ def get_option_chain(yf_obj):
     option_chain = DDict()
     ticker_data = yf_obj
     expirations = ticker_data.options
+
+    risk_free_interest = 1
+
+    def populate_call_greeks(contracts, dte):
+        contracts["delta"] = 0
+        contracts["gamma"] = 0
+        contracts["vega"] = 0
+        contracts["rho"] = 0
+        contracts["theta"] = 0
+
+        for contract in contracts:
+            call_price = (contract["Bid"] + contract["Ask"]) / 2
+            bs_obj = mibian.BS([ticker_data.info.ask,
+                                contract["Strike Price"],
+                                risk_free_interest,
+                                dte],
+                               volatility=contract["Implied volatility"],
+                               callPrice=call_price)
+
+            contract["delta"] = bs_obj.callDelta
+            contract["gamma"] = bs_obj.gamma
+            contract["vega"] = bs_obj.vega
+            contract["theta"] = bs_obj.callTheta
+            contract["rho"] = bs_obj.callRho
+
+    def populate_put_greeks(contracts, dte):
+        contracts["delta"] = 0
+        contracts["gamma"] = 0
+        contracts["vega"] = 0
+        contracts["rho"] = 0
+        contracts["theta"] = 0
+
+        for contract in contracts:
+            put_price = (contract["Bid"] + contract["Ask"]) / 2
+            bs_obj = mibian.BS([ticker_data.info.ask,
+                                contract["Strike Price"],
+                                risk_free_interest,
+                                dte],
+                               volatility=contract["Implied volatility"],
+                               putPrice=put_price)
+
+            contract["delta"] = bs_obj.putDelta
+            contract["gamma"] = bs_obj.gamma
+            contract["vega"] = bs_obj.vega
+            contract["theta"] = bs_obj.putTheta
+            contract["rho"] = bs_obj.putRho
+
     for expiration in expirations:
-        option_chain[expiration] = ticker_data.option_chain(expiration)
-        # todo: calc greeks and add them
+        chain = ticker_data.option_chain(expiration)
+        calls = chain.calls
+        puts = chain.puts
+
 
     return option_chain
 
@@ -148,15 +199,21 @@ class DDict(dict):
 def high(x):  # x 0 to 100, high if > 50; returns scale from 0 to 1 indicating highness
     return 0 if x <= 50 else (x / 50 if x > 0 else 0) - 1
 
+
 def low(x):
     return 0 if x >= 50 else 1 - (x / 50 if x > 0 else 0)
+
 
 def calendar_check(front_m_vol, back_m_vol):
     return front_m_vol >= back_m_vol * 1.2
 
+
 def best_theta_dte(dtes):
-    return min([45-dte for dte in dtes])
+    return min([45 - dte for dte in dtes])
+
+
 # </editor-fold>
+
 
 class OptionStrategy:
 
@@ -173,6 +230,10 @@ class LongCall(OptionStrategy):
 
     def test(self, params):
         # option chain, u price, short, mid, long outlook (-1 - 1),
-        ...
+        if params.short_outlook > 0.8:
+            # which delta call to choose?
+            ...
 
-pp(get_options_data("AAPL"))
+
+df = yf.Ticker("FREQ").option_chain(date="2021-05-21").puts
+pp(df.loc[df['strike'] == 30])
